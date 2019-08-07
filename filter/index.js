d4c45f6b7108f326b5e6eb36e784fdda7dcef5af
andrew hsieh <andrewhsieh1208@gmail.com>
2019-08-07 08:12:04 -0400

    exercises

---
+++ filter/index.js
@@ -0,0 +1,82 @@
+const showIt = function(scenario, anyObject) {
+  console.log(`Scenario: ${scenario}`);
+  console.log(JSON.stringify(anyObject, null, 2));
+  console.log("################################");
+}
+
+// same drill as last time, only for loops
+
+const grades = [70, 50, 70, 24, 77, 93, 99, 82, 42, 59, 73]
+
+const students = [
+  {
+    name: "lil jon",
+    age: 2,
+    id: 1431,
+    gender: "male",
+    major: "music",
+    enrolled: false
+  },
+  {
+    name: "bill blanks",
+    age: 25,
+    id: 203,
+    gender: "male",
+    major: "environmental engineering",
+    enrolled: true
+  },
+  {
+    name: "steve woz",
+    age: 104,
+    id: 8869,
+    gender: "male",
+    major: "computer engineering",
+    enrolled: false
+  },
+  {
+    name: "michelle chen",
+    age: 22,
+    id: 428,
+    gender: "female",
+    major: "economics",
+    enrolled: true
+  }
+]
+
+const teachers = [
+  {
+    name: "jbiebs",
+    age: 22,
+    id: 1888,
+    gender: "male",
+    subject: "statistics",
+    department: "mathematics"
+  },
+  {
+    name: "cameron",
+    age: 60,
+    id: 1000,
+    gender: "female",
+    subject: "gender studies",
+    department: "humanities"
+
+  },
+  {
+    name: "jenny yoon",
+    age: 34,
+    id: 5444,
+    gender: "male",
+    subject: "mathematics",
+    department: "humanities"
+  },
+]
+
+function makePassingList() {
+  
+}
+
+const checkIt = function() {
+  // showIt("copy-01", makePassingList(grades));
+}
+
+checkIt();
---
+++ reduce/index.js
@@ -0,0 +1,67 @@
+// for loop
+
+const successes = [true, false, true, true, false, true, true]
+const grades = [70, 50, 70, 24, 77, 93, 99, 82]
+
+const students = [
+  {
+    name: "lil jon",
+    age: 2,
+    id: 1431,
+    gender: "male",
+    major: "music",
+    enrolled: false
+  },
+  {
+    name: "bill blanks",
+    age: 25,
+    id: 203,
+    gender: "male",
+    major: "environmental engineering",
+    enrolled: true
+  },
+  {
+    name: "steve woz",
+    age: 104,
+    id: 8869,
+    gender: "male",
+    major: "computer engineering",
+    enrolled: false
+  },
+  {
+    name: "michelle chen",
+    age: 22,
+    id: 428,
+    gender: "female",
+    major: "economics",
+    enrolled: true
+  }
+]
+
+const teachers = [
+  {
+    name: "jbiebs",
+    age: 22,
+    id: 1888,
+    gender: "male",
+    subject: "statistics",
+    department: "mathematics"
+  },
+  {
+    name: "cameron",
+    age: 60,
+    id: 1000,
+    gender: "female",
+    subject: "gender studies",
+    department: "humanities"
+
+  },
+  {
+    name: "jenny yoon",
+    age: 34,
+    id: 5444,
+    gender: "male",
+    subject: "mathematics",
+    department: "humanities"
+  },
+]
